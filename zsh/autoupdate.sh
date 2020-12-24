8eef9775aab3a73bc8a2f0bd4ba2156936e3aa1e
Vitaly Kolmakov <w.kolmakow@gmail.com>
2020-12-24 14:09:44 +0100

    zsh: autoupdate fixed and new check for uncommited local changes in this repo

--- zsh/autoupdate.sh
+++ zsh/autoupdate.sh
@@ -2,7 +2,15 @@
 
 INSTALL_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $INSTALL_DIR
-        
+
+# inform about local changes, update will not be possible before commit
+if ! git diff-index --quiet HEAD --; then
+	echo -e "${RED}You have uncommited changes in linuxEnv repo ($(dirname $(pwd)))${NC}"
+	echo "Auto update is impossible until you commit or drop following changes:"
+	git status -s
+	exit 1 	
+fi
+
 # update once a day
 UPDATED=$(cat updated.date 2>/dev/null)
 NOW=$(date +%Y-%m-%d)
@@ -15,9 +23,8 @@ git pull --rebase | grep 'is up to date.' &> /dev/null
 if [ $? == 0 ]; then
    echo "nothing to update!"
 else 
-	../install.sh -f
+	../install.sh update 
 	echo -e "\nUpdate finished!"
 fi
 
 echo $NOW > updated.date
-
